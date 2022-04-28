c6d4ac9a46041aec3d833d70e6d45acb95dddc5a
Tanya <tanaybrown@gmail.com>
2022-04-28 14:11:53 -0700

    Completed assigbment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let form = document.querySelector("form")
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+ buildATask(e.target.querySelector('#new-task-description').value)
+  })
+})
